e06834ccf704e29181a011a0eb6b9609607ecfb8
Max43807 <83298505+Max43807@users.noreply.github.com>
2023-05-05 13:21:21 -0300

    Agregue varios componentes a la app

--- FrontEnd-Proyecto/src/app/app.module.ts
+++ FrontEnd-Proyecto/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
